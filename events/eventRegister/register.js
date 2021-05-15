9a08f7f2c56868dada5d751663b0f9913993b934
prophet-x <57630117+prophet-x@users.noreply.github.com>
2021-05-15 13:50:51 +0530

    Update register.js

--- events/eventRegister/register.js
+++ events/eventRegister/register.js
@@ -1,45 +1,46 @@
 var eventArray = [
-    "Code Red", // 0
-    "Bug off", // 1
-    "Reverse coding", // 2
-    "only girls coding event", // 3
+  "Code Red", // 0
+  "Bug off", // 1
+  "Reverse coding", // 2
+  "only girls coding event", // 3
 
-    
-    "NAND IT", // 4
-    "IMPEDENCE", // 5
-
-
-    "Catia" , // 6
-    "AutoCAD", // 7
-
- 
-    "CODM (BR & MP)", // 8
-    "Valorant", // 9
-    "clash royal", // 10
-    "Free fire", // 11
+  
+  "NAND IT", // 4
+  "IMPEDENCE", // 5
 
 
-    "Tech matrix", // 12
-    "Sudoku tech", // 13
-    "placement APTI", // 14
-    "Rubix Cube", // 15
-    "photoshop event for poster", // 16
-    "Ad making" , // 17
-    "photography", // 18
+  "Catia" , // 6
+  "AutoCAD", // 7
 
 
-    "Robotics club event 1",  // 19
-    "Robotics club event 2", // 20
-    "Robotics club event 3", // 21
-
- 
-    "E -cell event 1", // 22
-    "E -cell event 2", // 23
+  "CODM (BR & MP)", // 8
+  "Valorant", // 9
+  "clash royal", // 10
+  "Free fire", // 11
 
 
-    "ISDF event 1", // 24
-   ];
+  "Tech matrix", // 12
+  "Sudoku tech", // 13
+  "placement APTI", // 14
+  "Rubix Cube", // 15
+  "photoshop event for poster", // 16
+  "Ad making" , // 17
+  "photography", // 18
+  "Tech storm", // 26
 
+
+  "Robotics club event 1",  // 19
+  "Robotics club event 2", // 20
+  "Robotics club event 3", // 21
+  "Robotics club event 4", // 25
+
+
+  "E -cell event 1", // 22
+  "E -cell event 2", // 23
+
+
+  "ISDF event 1", // 24
+ ];
 
 //getting course params 
 function getUrlVars() {
